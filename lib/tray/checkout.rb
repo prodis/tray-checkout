77e41a8762a08fe98d7001441d9a215259ddb8aa
Prodis <prodis@gmail.com>
2012-12-28 09:11:42 -0200

    Move Timeout module to a new file.

--- lib/tray/checkout.rb
+++ lib/tray/checkout.rb
@@ -4,16 +4,6 @@ require 'log-me'
 module Tray
   module Checkout
     extend LogMe
-
-    module Timeout
-      DEFAULT_REQUEST_TIMEOUT = 5 #seconds
-      attr_writer :request_timeout
-
-      def request_timeout
-        (@request_timeout ||= DEFAULT_REQUEST_TIMEOUT).to_i
-      end
-    end
-
     extend Timeout
   end
 end
