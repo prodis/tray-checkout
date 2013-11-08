c0e33fb330b0aa5630b97c3560557778cc06ffca
Caio Torres <caio.torres@locaweb.com.br>
2013-11-08 13:46:58 -0200

    Adjusted cart_url method in temp_transaction and fixed README

--- lib/tray/checkout/config.rb
+++ lib/tray/checkout/config.rb
@@ -21,14 +21,6 @@ module Tray
         end
       end
 
-      def cart_url
-        if environment == :production
-          "http://checkout.tray.com.br/"
-        else
-          "http://checkout.sandbox.tray.com.br/"
-        end
-      end
-
       def request_timeout
         (@request_timeout ||= DEFAULT_REQUEST_TIMEOUT).to_i
       end
